2fe62e4e0145e7f083f2b17060baee91e759555a
PaulYH <paulhumenny@icloud.com>
2024-02-14 18:25:58 -0500

    CMS-92 Setup basic authentication logic and endpoints (with files this time)

---
+++ server/CMS/Data/CMSDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CMS.Api.UserSystem.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Api.Data
+{
+    public class CMSDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CMSDbContext(DbContextOptions<CMSDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ server/CMS/UserSystem/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CMS.Api.UserSystem.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+    }
+}
