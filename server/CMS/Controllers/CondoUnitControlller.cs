214eb0fd066cc5e2a6860d88249dee5ff2de7d16
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-02-22 05:59:00 -0500

    CondoUnit backend

---
+++ server/CMS/Controllers/CondoUnitControlller.cs
@@ -0,0 +1,73 @@
+﻿using CMS.Api.UserSystem.Entities;
+using CMS.Api.UserSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using CMS.Api.PropertySystem;
+using CMS.Api.PropertySystem.Services;
+using CMS.Api.PropertySystem.Entities;
+
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class CondoUnitControlller : ControllerBase
+    {
+        private readonly ICondoUnitService _condoUnitService;
+        private readonly IApplicationUserService _applicationUserService;
+        public CondoUnitControlller(ICondoUnitService condoUnitService) 
+        {
+            _condoUnitService = condoUnitService;
+        }
+
+        [HttpGet("{email}")]
+        public async Task<ActionResult<CondoUnit>> GetCondoUnitsByEmail(string email)
+        {
+            var condos = await _condoUnitService.GetCondoUnitsByEmail(email);
+            if (condos == null)  return NotFound(); 
+            return Ok(condos);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CondoUnit>> CreateCondoUnit([FromBody] CreateCondoUnitRequest request)
+        {
+            CondoUnit condoUnit = new CondoUnit();
+            condoUnit.ExternalUnitId = request.ExternalUnitId;
+            condoUnit.Size = request.Size;
+            condoUnit.FeePerSquareFoot = request.FeetPerSquareFoot;
+
+            var owner = await _applicationUserService.GetUserByEmail(request.CondoOwnerEmail);
+            var occupant = await _applicationUserService.GetUserByEmail(request.CondoOccupantEmail);
+
+            condoUnit.Owner = owner.Value;
+            condoUnit.Occupant = occupant.Value;
+
+
+
+            return await _condoUnitService.CreateCondoUnit(condoUnit);
+        }
+
+        [HttpDelete]
+        public async Task<bool> DeleteCondoUnit(int id)
+        {
+            return await _condoUnitService.DeleteCondoUnit(id);
+        }
+
+
+        public class CreateCondoUnitRequest
+        {
+            public int ExternalUnitId { get; set; }
+            public int Size { get; set; }
+            public decimal FeetPerSquareFoot { get; set; }
+            public required string CondoOwnerEmail { get; set; }
+            public required string CondoOccupantEmail { get; set; }
+
+            /* should you make the owner id and occupand id an attribute here */
+
+        }
+
+
+    }
+}
---
+++ server/CMS/PropertySystem/Services/ICondoUnitService.cs
@@ -0,0 +1,19 @@
+﻿using CMS.Api.PropertySystem.Entities;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+
+namespace CMS.Api.PropertySystem.Services
+{
+    public interface ICondoUnitService
+    {
+        Task<ActionResult<List<CondoUnit>>> GetCondoUnitsByEmail(string email);
+        Task<ActionResult<CondoUnit>> CreateCondoUnit(CondoUnit condoUnit);
+        Task<bool> DeleteCondoUnit(int id);
+
+    }
+}
