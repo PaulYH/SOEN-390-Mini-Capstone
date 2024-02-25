a2b4406eb3f7ad0818aae9be1d674113a6573894
Christa <Christa.arraj@gmail.com>
2024-02-25 09:55:45 -0500

    CMS-51 fixed the amenities page

--- client/src/containers/OwnerDashboard/Amenities.js
+++ client/src/containers/OwnerDashboard/Amenities.js
@@ -1,16 +1,20 @@
+/* this page is common to owners and rental users*/
+
 import React from 'react';
+//import { Link } from 'react-router-dom';
 import './Amenities.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Amenities = () => {
 
   return (
     <div className="amenities-container">
-      <div className="amenities-header">
-      <img src={require('../../assets/logo.png')} alt="logo" className="logo" />
+      {/* <div className="amenities-header"> */}
+      <img src={require('../../assets/logo.png')} alt="logo" className="logo"/>
         <h1>My Amenities</h1>
-      </div>
-      <div className="amenities-list">
+      {/* </div> */}
+      {/* <div className="amenities-list">
         <div className="amenity-item">
           <h2 style={{display: 'inline'}}>Gym & Spa</h2>&nbsp;
           <span class="badge bg-dark text-light">Open 24/7</span>
@@ -27,12 +31,64 @@ const Amenities = () => {
           <h2 style={{display: 'inline'}}>Outdoor playground</h2>&nbsp;
          <span class="badge bg-dark text-light">Open 24/7</span>
         </div>
+      </div> */}
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={require('../../assets/indoor-pool-children.jpg')} class="d-block w-100" alt="indoor kids pool"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Indoor kids Pool</h5>
+        <p>Open Everyday 24/7</p>
       </div>
-      
     </div>
-
-
-
+    <div class="carousel-item">
+    <img src={require('../../assets/indoor-pool-adult.jpg')} class="d-block w-100" alt="indoor adult pool"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Indoor Adult Pool (16+) </h5>
+        <p>Open Everyday 24/7</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={require('../../assets/spa.jpg')} class="d-block w-100" alt="spa"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Spa (16+)</h5>
+        <p>Open Monday to Saturday: 10am to 10pm</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={require('../../assets/outdoor-playground.jpg')} class="d-block w-100" alt="outdoor playground"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Outdoor Playground & Basketball Court</h5>
+        <p>Open Everyday 24/7</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={require('../../assets/sky-lounge.jpg')} class="d-block w-100" alt="lounge"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Sky Lounge</h5>
+        <p> Restaurant: Everyday from 10am to 9pm </p>
+        <p> Bar (18+): Everyday from 9pm to 3am </p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
   );
 };
 
--- client/src/containers/OwnerDashboard/SubmittedRequests.js
+++ client/src/containers/OwnerDashboard/SubmittedRequests.js
@@ -4,32 +4,69 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
-const SubmittedRequests = () => {}
+const SubmittedRequests = () => {
 return (
+        <div className="requests">
+
+{
+        // navbar
+      }
 
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="ownerFinance">Dashboard</a> 
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link" href="#">Settings</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Calendar</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Submit a Request </a> 
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  {
+        // body
+      }
+
+  <img src={require('../../assets/logo.png')} alt="logo" className="logo" />
     <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+        Request 1- <span class="date"> 1/1/2024</span>
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>Description of the request</strong> 
+        
+        <p>It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow. </p>
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+        Request 2
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <strong>This is the second item's accordion body.</strong> <br/> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
       </div>
     </div>
   </div>
@@ -46,5 +83,27 @@ return (
     </div>
   </div>
 </div>
-)
+
+
+    {
+    // pagination
+    }
+
+    <nav aria-label="Page navigation example">
+    <ul className="pagination justify-content-center">
+      <li className="page-item">
+        <Link className="page-link" to="/ownerFinance">1</Link>
+      </li>
+      <li className="page-item">
+        <Link className="page-link" to="/submittedRequests">2</Link>
+      </li>
+      <li className="page-item">
+        <Link className="page-link" to="/amenities">3</Link>
+      </li>
+      
+    </ul>
+  </nav> 
+  </div>
+);
+};
 export default SubmittedRequests; 
