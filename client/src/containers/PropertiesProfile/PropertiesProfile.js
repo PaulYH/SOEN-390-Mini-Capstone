95664a941f887224992b3f6948eaec19b3530e35
jack spiratos <jspiratos26@gmail.com>
2024-02-25 12:46:08 -0500

    attempt at api

--- client/src/containers/PropertiesProfile/PropertiesProfile.js
+++ client/src/containers/PropertiesProfile/PropertiesProfile.js
@@ -16,6 +16,15 @@ const PropertiesProfile = () => {
     const NavigateParkingLocker = () => {
         navigate('/parkinglocker');
     };
+
+    // const handleFileUpload = async (propertyId) => {
+
+    //     try {
+    //         const fileUpload = await fetch('http://localhost:5127/api/properties/{id}/upload',
+    //              )
+    //     }
+
+    // }
     
     const addPropertyToUser = async (propertyId) => {
         try {
@@ -113,7 +122,7 @@ const PropertiesProfile = () => {
             <input type="text" value={City} onChange={e => setCity(e.target.value)} />
             
             <div className="buttonDiv">
-            <button className='buttonUpload'>
+            <button className='buttonUpload' onClick={handleFileUpload}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 3H12H8C6.34315 3 5 4.34315 5 6V18C5 19.6569 6.34315 21 8 21H11M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V11.8125" stroke="#fffffff" stroke-width="2"></path>
             <path d="M17 15V18M17 21V18M17 18H14M17 18H20" stroke="#fffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
