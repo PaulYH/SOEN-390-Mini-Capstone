5d3f87fd951c37c8dac1d71293aafc2f82d2c6b0
ckdu <51294469+ckdu@users.noreply.github.com>
2024-03-02 23:49:09 -0500

    move folder for organisation

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less|scss|sass)$': '<rootDir>/tests/__mocks__/styleMock.js',
+        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/tests/__mocks__/fileMock.js',
     },
 };
