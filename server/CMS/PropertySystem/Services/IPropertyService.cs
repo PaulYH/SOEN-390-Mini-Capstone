2b74a51467c2d7fd730611fd84ffb073535af3fb
PaulYH <paulhumenny@icloud.com>
2024-03-03 09:10:42 -0500

    Tweak post-merge errorrs and basic UI issues

--- server/CMS/PropertySystem/Services/IPropertyService.cs
+++ server/CMS/PropertySystem/Services/IPropertyService.cs
@@ -9,7 +9,7 @@ namespace CMS.Api.PropertySystem.Services
         Task<ActionResult<List<Property>>> GetAllProperties();
         Task<ActionResult<Property>> GetPropertyById(Guid id);
         Task<ActionResult<Property>> CreateProperty(Property property);
-        Task<ActionResult<Property>> UpdatePropertyProfile(Property updatedProperty);
+        Task<ActionResult<Property>> UpdateProperty(Property updatedProperty);
         Task<ActionResult<CondoUnit>> AssociateCondoUnitWithProperty(Guid propertyId, Guid condoId);
         Task<ActionResult<bool>> DeleteProperty(Guid id);
         Task<ActionResult<List<CondoUnit>>> GetAllCondoUnits(Guid id);
