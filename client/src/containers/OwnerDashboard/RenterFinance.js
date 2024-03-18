8ccc4fad8ba329f06294d48bc721b982462fcbe4
ckdu <51294469+ckdu@users.noreply.github.com>
2024-03-17 23:07:26 -0400

    refactor RenterFinance

--- client/src/containers/OwnerDashboard/RenterFinance.js
+++ client/src/containers/OwnerDashboard/RenterFinance.js
@@ -1,105 +1,64 @@
-/* Finance page of owners */
-
-import React, { useState, useEffect } from 'react';
-import styles from './RenterFinance.module.css';
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
+import styles from './RenterFinance.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap';
-import  { ReactComponent as Download } from 'bootstrap-icons/icons/box-arrow-down.svg';
-
-
+import { ReactComponent as Download } from 'bootstrap-icons/icons/box-arrow-down.svg';
+import axios from 'axios';
 
 const userData = {
-    
-    companyName: 'Doe Enterprises',
-    condoNumber: '1234',
-    rent: 3000,
-    lastPaymentDate: 'January 1st, 2024',
+  companyName: 'Doe Enterprises',
+  condoNumber: '1234',
+  rent: 3000,
+  lastPaymentDate: 'January 1st, 2024',
+};
+
+const RenterFinanceDashboard = () => {
+  const navigate = useNavigate();
+
+  const fetchUserInfo = async () => {
+    const response = await axios.get('http://localhost:5127/api/users/authenticated', {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    });
+    return response.data.value;
   };
-  
 
-  
-  const RenterFinanceDashboard = () => {
-   
-    let navigate = useNavigate();
-    const handleClick = () => {
-      navigate('/home'); 
-    };
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [companyName, setCompanyName]=useState('');
-  
-    useEffect(() => {
-      fetchUserInfo();
-    }, []);
-  
-    const fetchUserInfo = async () => {
-      try {
-        const response = await fetch('http://localhost:5127/api/users/authenticated', {
-          headers: {
-            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-          },
-        });
-        if (!response.ok) throw new Error('Failed to fetch user data');
-        const userData = await response.json();
-        setFirstName(userData.value.firstName);
-        setLastName(userData.value.lastName);
-        setCompanyName(userData.value.property.companyName)
-      } catch (error) {
-        console.error(error);
-    
-      }
-    };
+  const { data: user, isLoading, isError } = useQuery(['userInfo'], fetchUserInfo);
 
+  const handleClick = () => {
+    navigate('/home');
+  };
 
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching user data.</div>;
 
-    return (
-      
-      <>
-     
-     <button type="button" className={styles.button} onClick={handleClick}>Back</button> 
-      
+  return (
+    <>
+      <button type="button" className={styles.button} onClick={handleClick}>Back</button>
       <div className="d-flex justify-content-center my-4" style={{marginLeft:'40px'}}>
-     
-    
-      <img src={require('../../assets/logo.png')} alt="logo"/></div>
+        <img src={require('../../assets/logo.png')} alt="logo"/>
+      </div>
 
-
-{
-        // body
-      }
-      
-      <h1 className="ms-5" style={{color:'black', marginBottom:'-20px'}}>Finances</h1>
-        <div className={styles.dashboardHeader}>
-        
-        
+      <h1 className="ms-5" style={{color: 'black', marginBottom: '-20px'}}>Finances</h1>
+      <div className={styles.dashboardHeader}>
         <div className="row">
           <div className="col-lg-12 col-md-12 ms-4 me-4 d-flex justify-content-center">
-        <div className="card d-flex justify-content-center" style={{width:'70%'}}>
-          {/* <h2 className={styles.Finances}>My Finances</h2> */}
-          {/* <div className="text-muted" style={{textAlign:'left'}}>Condo Owner:<span className="badge badge-size bg-light text-dark">{`${firstName} ${lastName}`}</span> </div>
-          <div className="text-muted" style={{textAlign:'left'}}>Company name:<span className="badge badge-size bg-light text-dark">{companyName}</span> </div>
-          <div className="text-muted" style={{textAlign:'left'}}>Condo Number: <span className="badge badge-size bg-light text-dark">#1234</span></div> */}
-          
-<div className="card-body">
-            <span className="d-inline-block" tabindex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Disabled popover">
-  <button className={`btn ${styles.disabledButton}`} type="button" disabled >Rent: {userData.rent}$</button>
-</span>
-  
-          <div className="m-3" >Last payment made on: {userData.lastPaymentDate} <br />
-          <button type="button" className="btn btn-outline-primary m-3">Download Invoice <Download/></button>
+            <div className="card d-flex justify-content-center" style={{width:'70%'}}>
+              <div className="card-body">
+                <span className="d-inline-block" tabIndex="0" data-bs-toggle="popover" data-bs-trigger="hover focus" data-bs-content="Disabled popover">
+                  <button className={`btn ${styles.disabledButton}`} type="button" disabled>Rent: ${userData.rent}</button>
+                </span>
+                <div className="m-3">Last payment made on: {userData.lastPaymentDate}<br/>
+                  <button type="button" className="btn btn-outline-primary m-3">Download Invoice <Download /></button>
+                </div>
+              </div>
+            </div>
           </div>
-          </div>
-          </div>
-          </div>
-          </div>
-        
-       </div>
-
-        
-         
-      
-  
+        </div>
+      </div>
     </>
   );
 };
