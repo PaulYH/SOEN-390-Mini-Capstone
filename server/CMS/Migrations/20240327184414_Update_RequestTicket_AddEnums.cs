0a27c26a72bd419c306fe3db7c3ae9677284cef0
PaulYH <paulhumenny@icloud.com>
2024-03-27 14:44:50 -0400

    Add enums to RequestTicket

---
+++ server/CMS/Migrations/20240327184414_Update_RequestTicket_AddEnums.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_RequestTicket_AddEnums : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryType",
+                table: "RequestTickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "RequestTickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryType",
+                table: "RequestTickets");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "RequestTickets");
+        }
+    }
+}
