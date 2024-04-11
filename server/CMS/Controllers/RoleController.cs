029f5fe934f44d5e93bb7d854216b0521905d73a
PaulYH <paulhumenny@icloud.com>
2024-04-11 09:38:00 -0400

    CMS-110 Add RoleController with GetUserRole endpoint

---
+++ server/CMS/Controllers/RoleController.cs
@@ -0,0 +1,36 @@
+﻿using CMS.Api.UserSystem.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/role")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public RoleController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> GetUserRole(string id)
+        {
+            var user = _userManager.Users.Where(x => x.Id == id).FirstOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var role = await _userManager.GetRolesAsync(user);
+            if (role == null || role.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return role.First();
+        }
+    }
+}
