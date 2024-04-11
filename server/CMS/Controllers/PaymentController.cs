b823d96201d2497da12cb5827647557889c9c96c
hisham-kitaz <111257875+hisham-kitaz@users.noreply.github.com>
2024-04-11 19:39:06 -0400

    Created PaymentServices , PaymentController. IPaymentServices, SystemTimeSingleton.

---
+++ server/CMS/Controllers/PaymentController.cs
@@ -0,0 +1,79 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using CMS.Api.PropertySystem.Services;
+using CMS.Api.FinancialSystem;
+using CMS.Api.UserSystem.Services;
+using CMS.Api.FinancialSystem.Services;
+using CMS.Api.UserSystem.Entities;
+
+namespace CMS.Api.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _paymentService;
+        private readonly IApplicationUserService _applicationUserService;
+        private readonly IPropertyService _propertyService;
+        private readonly ICondoUnitService _condoUnitService;
+        private readonly IParkingSpotService _parkingSpotService;
+        private readonly ILockerService _lockerService;
+
+        public PaymentController(IPaymentService paymentService, IApplicationUserService applicationUserService,
+                       IPropertyService propertyService, ICondoUnitService condoUnitService, IParkingSpotService parkingSpotService,
+                                  ILockerService lockerService)
+        {
+            _paymentService = paymentService;
+            _applicationUserService = applicationUserService;
+            _propertyService = propertyService;
+            _condoUnitService = condoUnitService;
+            _parkingSpotService = parkingSpotService;
+            _lockerService = lockerService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Payment>>> GetAllPayments()
+        {
+            var payments = await _paymentService.GetAllPayments();
+            return Ok(payments);
+        }
+        [HttpPost]//get back and change the timing
+        public async Task<ActionResult<Payment>> CreatePayment(Payment request, string userId)
+        {
+            Payment payment = new Payment();
+            payment.Amount = request.Amount;
+            ActionResult<ApplicationUser> result = await _applicationUserService.GetUserById(userId);
+                if (result.Value == null) return NotFound("User Not Found!");
+            payment.User = result.Value;
+            var createdPayment = await _paymentService.CreatePayment(payment, userId);
+            return Ok(createdPayment);
+        }
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<List<Payment>>> GetPaymentsOfUser(string userId)
+        {
+            var payments = await _paymentService.GetPaymentsOfUser(userId);
+            if (payments.Value == null) return NotFound();
+            return Ok(payments);
+        }
+        [HttpPut]
+        public async Task<ActionResult<ApplicationUser>> UpdateUserBalance(string userId, double updatedBalance)
+        {
+            var user = await _paymentService.UpdateUserBalance(userId, updatedBalance);
+            if (user.Value == null) return NotFound();
+            return Ok(user);
+        }
+        [HttpGet("/balance/{userId}")]
+        public async Task<ActionResult<double>> GetUserBalance(string userId)
+        {
+            var balance = await _paymentService.GetUserBalance(userId);
+            if (balance.Value == 0) return NotFound();
+            return Ok(balance);
+        }
+        [HttpGet("/report/{year}")]
+        public async Task<ActionResult<string>> GenerateAndDownloadAnnualReport(int year)
+        {
+            var report = await _paymentService.GenerateAndDownloadAnnualReport(year);
+            if (report.Value == null) return NotFound();
+            return Ok(report);
+        }
+    }
+}
---
+++ server/CMS/SystemTimeSingleton.cs
@@ -0,0 +1,53 @@
+﻿using CMS.Api.Data;
+using System.Timers;
+namespace CMS.Api
+
+{
+    public class SystemTimeSingleton
+    {
+        private static SystemTimeSingleton? instance ;
+        public DateTime CurrentTime =DateTime.Now;
+        private System.Timers.Timer timer;
+        private int timerInterval = 1000;
+        public int TimeSpeed = 1;
+        private readonly CMSDbContext _context;
+        private SystemTimeSingleton() {}
+       
+        public static SystemTimeSingleton GetInstance()
+        {
+                if (instance == null)
+                {
+                    instance = new SystemTimeSingleton();
+                    instance.CreateTimer();
+                }
+                return instance;
+        
+        }
+        private void CreateTimer()
+        {
+            timer = new System.Timers.Timer(timerInterval);
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Elapsed += (sender, e) => UpdateTime();
+        }
+        // timer logic here 
+        private void UpdateTime()
+        {
+              timer.Interval = timerInterval / TimeSpeed;
+              CurrentTime = CurrentTime.AddSeconds(1);
+            // so you can call it only one a month @ 2:00am
+            TimeSpan time = new TimeSpan(2, 0, 0);
+            // if the first of the month update balance
+            if (CurrentTime.Day == 1 && CurrentTime.TimeOfDay == time)
+            {
+                UpdateBalance();
+            }
+        }
+        public void UpdateBalance()
+        {
+            // Update balance would go over every user add up fees.  
+        }
+    }
+
+
+}
