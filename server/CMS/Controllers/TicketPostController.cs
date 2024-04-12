b92b5526451d2a3917344f3dc9169e5013668f23
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-04-12 02:34:08 -0400

    added TicketPostController.CreateTicketPost()

--- server/CMS/Controllers/TicketPostController.cs
+++ server/CMS/Controllers/TicketPostController.cs
@@ -5,6 +5,7 @@ using CMS.Api.PropertySystem.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using CMS.Api.RequestSystem.Services;
+using CMS.Api.RequestSystem.Entities;
 
 namespace CMS.Api.Controllers
 {
@@ -17,6 +18,13 @@ namespace CMS.Api.Controllers
         public TicketPostController(ITicketPostService ticketPostService)
         {
             _ticketPostService = ticketPostService;
+        }
+
+        public async Task<ActionResult<TicketPost>> CreateTicketPost(TicketPost request, string requestTicketId)
+        {
+            if (request is null || request.CreatedBy is null) { return BadRequest(); }
+            var createdPost = await _ticketPostService.CreateTicketPost(request, requestTicketId);
+            return Ok(createdPost);
         }
     }
 }
