bc502f39684bef255e7f1496bd3c7c6930458ebe
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-04-11 21:29:18 -0400

    Added IRequestTicketService.cs

---
+++ server/CMS/RequestSystem/Services/IRequestTicketService.cs
@@ -0,0 +1,12 @@
+﻿using CMS.Api.PropertySystem.Entities;
+using CMS.Api.RequestSystem.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Api.RequestSystem.Services
+{
+    public interface IRequestTicketService
+    {
+        Task<ActionResult<RequestTicket>> CreateRequestTicket(RequestTicket requestTicket);
+        //Task<ActionResult<RequestTicket>> UpdateRequestTicket(RequestTicket requestTicket);
+    }
+}
