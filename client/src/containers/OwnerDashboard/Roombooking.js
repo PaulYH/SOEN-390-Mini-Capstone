89b96b15d00e7de01ba2cd9cc089a1f2ef0f44a9
Christa <Christa.arraj@gmail.com>
2024-04-12 18:16:09 -0400

    test

--- client/src/containers/OwnerDashboard/Roombooking.js
+++ client/src/containers/OwnerDashboard/Roombooking.js
@@ -1,102 +1,100 @@
 //table for employees
 
 import React, { useState, useEffect } from 'react'
-
-import { useQuery, useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+// import { useQuery, useMutation } from '@tanstack/react-query'
+// import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import {Table, TableHeader, TableColumn, TableBody,Button, TableRow, TableCell, User, Chip, Tooltip, getKeyValue} from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, Button, TableRow, TableCell, User, Chip, Tooltip } from "@nextui-org/react";
+
 import {EditIcon} from "./EditIcon";
 import {DeleteIcon} from "./DeleteIcon";
-import {EyeIcon} from "./EyeIcon";
-import {columns, users} from "./data";
+
+import {columns, rooms} from "./dataEmployeeTable";
 
 
-  export default function Roombooking() {
+  const Roombooking = () =>{
 
     const themeMode = 'light' // for the components that are not set to a specific theme mode despite the global theme mode setting in index.js
     const navigate = useNavigate()
     // const { isOpen, onOpen, onOpenChange } = useDisclosure() // used to open/close Modal component
-    const [modalData, setModalData] = useState(null) // used to set the Modal's data to that of selected user
-    const [chosenUnit, setChosenUnit] = useState(null) // selected unit to assign to user
-    const [users, setUsers] = useState([]) 
+    // const [modalData, setModalData] = useState(null) // used to set the Modal's data to that of selected user
+    // const [chosenUnit, setChosenUnit] = useState(null) // selected unit to assign to user
+    // const [users, setUsers] = useState([]) 
   
-    const columns = [
-        // the columns used by Table component
-       
-        { name: 'ROOM', uid: 'room' },
-        { name: 'ExternalRoomID ', uid: 'id' },
-        { name: 'EDIT', uid: 'actions' } 
-        
-      ]
-
-
-const [addRoom, setRoom] = useState({
-    externalUnitID: '',
-    nameRoom:''
-  })
-
-
-  const fetchRooms = async () => {
-    const token = localStorage.getItem('accessToken');
-    const response = await fetch('http://localhost:5127/api/room', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-    if (!response.ok) {
-      throw new Error('Failed to fetch rooms');
-    }
-    return response.json();
-  };
   
-  const { data: userProfile, error: userProfileError } = useQuery({
-    queryKey: ['userProfile'],
-    queryFn: fetchRooms,
-    onSuccess: (data) => {
-      // Assuming data is an array of rooms
-      setRoom(data); // Assuming setRoom updates state with the fetched rooms
-    },
-    onError: (error) => {
-      console.error(error);
-    },
-  });
+
+
+// const [addRoom, setRoom] = useState({
+//     externalUnitID: '',
+//     nameRoom:''
+//   })
+
+
+  // const fetchRooms = async () => {
+  //   const token = localStorage.getItem('accessToken');
+  //   const response = await fetch('http://localhost:5127/api/room', {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+
+  //   if (!response.ok) {
+  //     throw new Error('Failed to fetch rooms');
+  //   }
+  //   return response.json();
+  // };
   
-      const renderCell = React.useCallback((user, columnKey) => {
-        const cellValue = user[columnKey];
+  // const { data: userProfile, error: userProfileError } = useQuery({
+  //   queryKey: ['userProfile'],
+  //   queryFn: fetchRooms,
+  //   onSuccess: (data) => {
+  //     // Assuming data is an array of rooms
+  //     setRoom(data); // Assuming setRoom updates state with the fetched rooms
+  //   },
+
+  //   onError: (error) => {
+  //     console.error(error);
+  //   },
+  // });
+  
+      const renderCell = React.useCallback((room, columnKey) => {
+        const cellValue = room[columnKey];
     
         switch (columnKey) {
           
-          case "id":
+          case "externalRoomId":
             return (
-              <div className="flex flex-col">
-                <p className="text-bold text-sm capitalize"></p>
-                <p className="text-bold text-sm capitalize text-default-400"></p>
-              </div>
+              // <div className="flex flex-col">
+              //   <p className="text-bold text-sm capitalize"></p>
+              //   <p className="text-bold text-sm capitalize text-default-400"></p>
+              // </div>
+              <User
+  
+            description={room.externalRoomId}
+            name={cellValue}
+          >
+            {room.externalRoomId}
+          </User>
             );
-            case "room":
+            case "name":
                 return (
                   <div className="flex flex-col">
-                    <p className="text-bold text-sm capitalize"></p>
-                    <p className="text-bold text-sm capitalize text-default-400"></p>
-                  </div>
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{room.name}</p>
+          </div>
                 );
           case "actions":
             return (
               <div className="relative flex items-center gap-2">
-                <Tooltip content="Details">
-                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EyeIcon />
-                  </span>
-                </Tooltip>
-                <Tooltip content="Edit user">
+               
+                <Tooltip content="edit room">
                   <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                     <EditIcon />
                   </span>
                 </Tooltip>
-                <Tooltip color="danger" content="Delete user">
+                <Tooltip content="Delete room">
                   <span className="text-lg text-danger cursor-pointer active:opacity-50">
                     <DeleteIcon />
                   </span>
@@ -136,9 +134,9 @@ const [addRoom, setRoom] = useState({
                   </TableColumn>
                 )}
               </TableHeader>
-              <TableBody items={users}>
+              <TableBody items={rooms}>
                 {(item) => (
-                  <TableRow key={item.id}>
+                  <TableRow key={item.externalRoomId}>
                     {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
                   </TableRow>
                 )}
@@ -148,7 +146,7 @@ const [addRoom, setRoom] = useState({
       );
       
 }
-
+export default Roombooking;
 //GET API call 
 
 
---
+++ client/src/containers/OwnerDashboard/dataEmployeeTable.js
@@ -0,0 +1,47 @@
+import React from "react";
+const columns = [
+    {name: "ROOM", uid: "name"},
+  {name: "ExternalRoomId", uid: "externalRoomId"},
+  {name: "ACTIONS", uid: "actions"},
+];
+
+const rooms = [
+  {
+    externalRoomId: 0,
+    name: "Kids pool",
+    
+  },
+  {
+    externalRoomId: 1,
+    name: "Adult Pool",
+    
+  },
+  {
+    externalRoomId: 2,
+    name: "Spa",
+    
+  },
+  {
+    externalRoomId: 3,
+    name: "Basketball court",
+    
+  },
+  {
+    externalRoomId: 4,
+    name: "Outdoor playground",
+    
+  },
+  {
+  externalRoomId: 5,
+    name: "Sky lounge",
+   
+  },
+  {
+  externalRoomId: 6,
+    name: "Restaurant",
+    
+  },
+  
+];
+
+export {columns, rooms};
