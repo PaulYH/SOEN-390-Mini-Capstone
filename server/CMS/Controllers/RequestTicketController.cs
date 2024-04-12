57807486da9a34c8bb1fb13bfe1317b16d408a33
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-04-11 22:02:23 -0400

    Added RequestTicketController.CreateRequestTicket() and UpdateRequestTicket()

--- server/CMS/Controllers/RequestTicketController.cs
+++ server/CMS/Controllers/RequestTicketController.cs
@@ -1,16 +1,41 @@
-﻿using CMS.Api.PropertySystem.Entities;
-using CMS.Api.UserSystem.Services;
+﻿using CMS.Api.RequestSystem.Entities;
+using CMS.Api.RequestSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Authorization;
-using CMS.Api.UserSystem.Enum;
-using CMS.Api.PropertySystem.Services;
+using CMS.Api.RequestSystem.Enums;
+
+using CMS.Api.RequestSystem.Services;
+using CMS.Api.PropertySystem.Entities;
 
 namespace CMS.Api.Controllers
 {
-    public class RequestTicketController
+    [Route("api/tickets")]
+    [ApiController]
+    public class RequestTicketController : ControllerBase
     {
+        private readonly IRequestTicketService _requestTicketService;
 
+        public RequestTicketController(IRequestTicketService requestTicketService)
+        {
+            _requestTicketService = requestTicketService;
+        }
+
+        [HttpPost] // Authorize owners/renters only
+        public async Task<ActionResult<Locker>> CreateRequestTicket(RequestTicket request)
+        {
+            if (request is null || request.CreatedBy is null) { return NotFound(); }
+            var createdTicket = await _requestTicketService.CreateRequestTicket(request);
+            return Ok(createdTicket);
+        }
+
+        [HttpPut] // Authorize Employees only
+        public async Task<ActionResult<RequestTicket>> UpdateRequestTicket(RequestTicket request)
+        {
+            if (request is null) { return NotFound(); }
+            var updatedTicket = await _requestTicketService.UpdateRequestTicket(request);
+            return Ok(updatedTicket);
+        }
     }
 }
