883b3d3e6ca6da3cd1e1e569b6030f9428971f6b
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-04-11 21:45:02 -0400

    Added RequestTicketController

---
+++ server/CMS/Controllers/RequestTicketController.cs
@@ -0,0 +1,16 @@
+﻿using CMS.Api.PropertySystem.Entities;
+using CMS.Api.UserSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using CMS.Api.UserSystem.Enum;
+using CMS.Api.PropertySystem.Services;
+
+namespace CMS.Api.Controllers
+{
+    public class RequestTicketController
+    {
+
+    }
+}
