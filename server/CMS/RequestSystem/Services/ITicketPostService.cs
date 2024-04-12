c7ec5389f59c0a599c8405309e3b9ed287856b01
etiennepaquet1 <107227178+etiennepaquet1@users.noreply.github.com>
2024-04-11 23:06:16 -0400

    added ITicketPostService.cs

---
+++ server/CMS/RequestSystem/Services/ITicketPostService.cs
@@ -0,0 +1,15 @@
+﻿using CMS.Api.PropertySystem.Entities;
+using CMS.Api.RequestSystem.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Api.RequestSystem.Services
+{
+    public interface ITicketPostService
+    {
+        Task<ActionResult<TicketPost>> CreateTicketPost(TicketPost ticketPost, string requestTicketId ); // accessible by owner and renter
+        Task<ActionResult<TicketPost>> ViewRequestTicket(RequestTicket request);
+        Task<ActionResult<IEnumerable<TicketPost>>> GetAll();
+
+
+    }
+}
