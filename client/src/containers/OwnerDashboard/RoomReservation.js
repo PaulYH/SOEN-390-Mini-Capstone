caeb57e0e4f0f39fdcb141d91ca26a6170befefb
Christa <Christa.arraj@gmail.com>
2024-04-12 12:47:22 -0400

    added dropdown

--- client/src/containers/OwnerDashboard/RoomReservation.js
+++ client/src/containers/OwnerDashboard/RoomReservation.js
@@ -1,19 +1,16 @@
 //User reserves room
 import React, { useState, useEffect } from 'react';
-import {ListboxWrapper} from "./ListboxWrapper";
-
 import {Button} from "@nextui-org/react";
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css"; 
-
+import "react-datepicker/dist/react-datepicker.css";
+import {Select, SelectItem} from "@nextui-org/react"; 
+import {timeSlot} from "./data";
 
 const RoomReservation = () => {
     let navigate = useNavigate();
-    const [startDate, setStartDate] = useState(new Date());
-   
+    
    /*Submit */
     const handleClick = () => { /*Api call to save user choice*/ 
         navigate('/amenities'); 
@@ -31,52 +28,134 @@ const RoomReservation = () => {
                     Kids pool
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"  defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2" >
                    Adult Pool
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                     Spa
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                     Basketball Court
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                    Outdoor Playground
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                     Sky Lounge
                 </label>
             </div>
+            <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
             <div className="form-check">
                 <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" defaultChecked />
                 <label className="form-check-label" htmlFor="flexRadioDefault2">
                     Restaurant
                 </label>
             </div>
-
-             {/* <select className='form-select' value={NReservations} onChange={(evt)=>setProduct({...product,productCategory:evt.target.value})}>
-            <option value="Kids pool">Kids Pool</option>
-            <option value="Adult Pool">Adult Pool</option>
-            <option value="Spa">Spa</option>
-            <option value="Outdoor Playground">Mobile</option>
-            <option value="Basketball Court">Basketball Court</option>
-            <option value="Sky Lounge">Sky Lounge</option>
-            <option value="Restaurant">Restaurant</option>
-            </select> */}
+          
+    <Select
+      isRequired
+      label="Time Slot"
+      placeholder="Select a time slot"
+      defaultSelectedKeys={["10am-11am"]}
+      className="max-w-xs"
+    >
+      {timeSlot.map((animal) => (
+        <SelectItem key={animal.value} value={animal.value}>
+          {animal.label}
+        </SelectItem>
+      ))}
+    </Select>
 
             <Button color="primary" onClick={handleClick}>
       Submit
--- client/src/containers/OwnerDashboard/data.js
+++ client/src/containers/OwnerDashboard/data.js
@@ -1,62 +1,11 @@
 import React from "react";
-const columns = [
-  {name: "NAME", uid: "name"},
-  {name: "ROLE", uid: "role"},
-  {name: "STATUS", uid: "status"},
-  {name: "ACTIONS", uid: "actions"},
+export const timeSlot = [
+  {label: "10am-12pm", value: "time1"},
+  {label: "12pm-2pm", value: "time2"},
+  {label: "2pm-4pm", value: "time3"},
+  {label: "4pm-6pm", value: "time4"},
+  {label: "6pm-8pm", value: "time5"},
+  {label: "8pm-10pm", value: "time6"},
+  {label: "10pm-12pm", value: "time7"},
+  
 ];
-
-const users = [
-  {
-    id: 1,
-    name: "Tony Reichert",
-    role: "CEO",
-    team: "Management",
-    status: "active",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    email: "tony.reichert@example.com",
-  },
-  {
-    id: 2,
-    name: "Zoey Lang",
-    role: "Technical Lead",
-    team: "Development",
-    status: "paused",
-    age: "25",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-    email: "zoey.lang@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Fisher",
-    role: "Senior Developer",
-    team: "Development",
-    status: "active",
-    age: "22",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-    email: "jane.fisher@example.com",
-  },
-  {
-    id: 4,
-    name: "William Howard",
-    role: "Community Manager",
-    team: "Marketing",
-    status: "vacation",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-    email: "william.howard@example.com",
-  },
-  {
-    id: 5,
-    name: "Kristen Copper",
-    role: "Sales Manager",
-    team: "Sales",
-    status: "active",
-    age: "24",
-    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-    email: "kristen.cooper@example.com",
-  },
-];
-
-export {columns, users};
