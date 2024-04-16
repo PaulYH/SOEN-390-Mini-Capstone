409eaaf1fef196f4575ab31e6ed9dbbdee103538
despinakouli <97748664+despinakouli@users.noreply.github.com>
2024-04-15 20:05:40 -0400

    CMS-87 get user role

--- client/src/containers/RequestBoard/ViewTicket.js
+++ client/src/containers/RequestBoard/ViewTicket.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const ViewTicket = () => {
 
   const { ticketId } = useParams();
   const navigate = useNavigate();
-  
+
   const [ticket, setTicket] = useState({
     title: '',
     description: '',
@@ -13,7 +14,29 @@ const ViewTicket = () => {
     creationDate: '',
     createdBy: ''
   });
+  const [userId, setUserId] = useState('');
+  const [userRole, setUserRole] = useState('');
 
+  useEffect(() => {
+    fetchUserInfo();
+  }, []);
+
+  //fetch user id
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get('http://localhost:5127/api/users/authenticated', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      });
+      setUserId(response.data.value.id);
+      console.log(userId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //fetching ticket
   useEffect(() => {
     const fetchTicket = async () => {
       try {
@@ -43,6 +66,38 @@ const ViewTicket = () => {
 
     fetchTicket();
   }, [ticketId]);
+
+  console.log("user id is", userId)
+
+
+    //fetching user role
+    useEffect(() => {
+        const fetchUserRole = async () => {
+          try {
+            const response = await fetch(`http://localhost:5127/api/role/${userId}`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+              }
+            });
+
+            const roleText = await response.text();
+            console.log(roleText);
+            console.log("user role is", roleText);
+            console.log(roleText);
+            console.log(roleText);
+            console.log(roleText);
+
+          } catch (error) {
+            console.error('Error fetching user role:', error);
+          }
+
+
+        };
+    
+        fetchUserRole();
+      }, []);
 
   const categoryLabel = (category) => {
     switch (category) {
