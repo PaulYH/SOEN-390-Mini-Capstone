d2864d8c59301061546214a8c9c5bcf465f36567
ckdu <51294469+ckdu@users.noreply.github.com>
2024-05-01 01:39:55 -0400

    fix page access to employee only

--- client/src/containers/OwnerDashboard/Amenities.js
+++ client/src/containers/OwnerDashboard/Amenities.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Amenities.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from '@nextui-org/react';
+import axios from 'axios';
 
 const Amenities = () => {
   let navigate = useNavigate();
+  const [userRole, setUserRole] = useState('');
+
+  const fetchUserRole = async () => {
+    try {
+      // First, fetch the authenticated user's ID
+      const userResponse = await axios.get('http://localhost:5127/api/users/authenticated', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      });
+
+      const userId = userResponse.data.value.id;
+
+      // Then, use the ID to fetch the user role
+      const roleResponse = await axios.get(`http://localhost:5127/api/role/${userId}`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      });
+      setUserRole(roleResponse.data.trim());
+    } catch (error) {
+      console.error('Error fetching user role:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserRole();
+  }, []);
+
   const handleClick = () => {
     navigate('/home');
   };
+  
   const handleClickCreate = () => {
     navigate('/roomReserve');
   };
+
   const handleClickSeeReservation = () => {
     navigate('/roombooking');
+  };
+
+  const handleGoToProfile = () => {
+    navigate('/profile');
   };
 
   return (
@@ -39,14 +75,24 @@ const Amenities = () => {
       </div>
       <div className={styles.darkOverlay}></div>
       <div className='containerd' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-        <img src={require('../../assets/logo.png')} alt='logo' className='mb-3' />
+        <img src={require('../../assets/logo.png')} alt='logo' className='mb-3'  onClick={handleClick}/>
         <h1 className={styles.whiteText} id='amenities'>Amenities</h1>
-        <Button color='primary' style={{ margin: '10px', fontSize: '20px', padding: '10px 20px' }} onClick={handleClickSeeReservation}>
-          Create Room
-        </Button>
-        <Button color='primary' style={{ margin: '10px', fontSize: '20px', padding: '10px 20px' }} onClick={handleClickCreate}>
-          Reserve
-        </Button>
+        {userRole === 'Employee' && (
+          <Button color='primary' style={{ margin: '10px', fontSize: '20px', padding: '10px 20px' }} onClick={handleClickSeeReservation}>
+            Create Room
+          </Button>
+        )}
+        {userRole === 'Renter' && (
+          <Button color='primary' style={{ margin: '10px', fontSize: '20px', padding: '10px 20px' }} onClick={handleClickCreate}>
+            Reserve
+          </Button>
+        )}
+        {userRole !== 'Employee' && userRole !== 'Renter' && (
+          <div>
+            <p>You must be a condo owner/renter to view this page, please request a condo key through the profile page.</p>
+            <Button onClick={handleGoToProfile}>Go to Profile</Button>
+          </div>
+        )}
       </div>
     </div>
   );
